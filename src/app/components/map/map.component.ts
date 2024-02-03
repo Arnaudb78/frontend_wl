62f3f5c181a793c13423ba42384661e2859e647f
Arnaudb78 <arnaudb92230@gmail.com>
2024-02-03 16:31:36 +0100

    WIP: add leaftlet library

---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,70 @@
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet'
+import "leaflet/dist/leaflet.css";
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent implements AfterViewInit {
+ // @ts-ignore
+ private map: L.Map; // Utilisation du type correct pour TypeScript
+
+ private smallIcon = new L.Icon({
+   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
+   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon-2x.png',
+   iconSize: [25, 41],
+   iconAnchor: [12, 41],
+   popupAnchor: [1, -34],
+   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+   shadowSize: [41, 41]
+ });
+
+ ngAfterViewInit(): void {
+   this.getCurrentLocation();
+ }
+
+ getCurrentLocation() {
+   if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition((position) => {
+       const coords = { lat: position.coords.latitude, lng: position.coords.longitude };
+       this.createMap(coords);
+       this.addMarker({ coords, text: "Vous êtes ici", open: true });
+     }, (error) => {
+       console.error('Error getting location', error);
+       // Fallback sur une position par défaut
+       this.createMap({ lat: 48.114384, lng: -1.669494 });
+     });
+   } else {
+     console.error("Geolocation is not supported by this browser.");
+     // Fallback sur une position par défaut
+     this.createMap({ lat: 48.114384, lng: -1.669494 });
+   }
+ }
+
+ createMap(coords: { lat: number; lng: number }) {
+   const zoomLevel = 17;
+
+   this.map = L.map('map', {
+     center: [coords.lat, coords.lng],
+     zoom: zoomLevel
+   });
+
+   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     minZoom: 12,
+     maxZoom: 17,
+     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+   }).addTo(this.map);
+ }
+
+ addMarker({ coords, text, open }: { coords: { lat: number; lng: number }; text: string; open: boolean }) {
+   const marker = L.marker([coords.lat, coords.lng], { icon: this.smallIcon });
+   marker.addTo(this.map).bindPopup(text, { autoClose: false });
+   if (open) {
+     marker.openPopup();
+   }
+ }
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { MapComponent } from '../../components/map/map.component';
 // import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, MapComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })

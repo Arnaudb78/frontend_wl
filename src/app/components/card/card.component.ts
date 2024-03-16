01e7301f357f5cb0af2f97a4ef2692e77e8ac416
nono <arnaudb92230@gmail.com>
2024-03-16 12:11:30 +0100

    WIP: get credential and send to back

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,6 +1,7 @@
 declare var google: any;
-import { Component, OnInit, inject } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -9,34 +10,20 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
-export class CardComponent implements OnInit {
-  private router = inject(Router);
-  ngOnInit(): void {
-    google.accounts.id.initialize({
-      client_id:
-        '863412296645-pdjr2r8oid6tppv09gtsoi7i9ff1qkb2.apps.googleusercontent.com',
-      callback: (resp: any) => this.handleLogin(resp),
-    });
-
-    google.accounts.id.renderButton(document.getElementById('google-btn'), {
-      theme: 'filled_blue',
-      size: 'large',
-      shape: 'rectangle',
-      width: 200,
-    });
-  }
-
-  private decodeToken(token: string) {
-    return JSON.parse(atob(token.split('.')[1]));
-  }
-
-  handleLogin(response: any) {
-    if (!response) throw new Error('No response from Google Accounts');
-    // decode the token to get the payload
-    const payLoad = this.decodeToken(response.credential);
-    // store the user in session storage
-    sessionStorage.setItem('loggedInUser', JSON.stringify(payLoad));
-    // navigate to the home page
-    this.router.navigate(['/home']);
+export class CardComponent {
+  constructor(private http: HttpClient) {}
+  async loginWithGoogle() {
+    console.log("Let's go back !!!");
+    try {
+      const code = 'test envoie back';
+      const response = await this.http
+        .post<any>('http://localhost:5000/google-auth', { code })
+        .toPromise();
+      console.log(response);
+      // Gérez la réponse de votre backend ici
+    } catch (error) {
+      console.error('Error during Google authentication:', error);
+      // Gérez l'erreur ici
+    }
   }
 }

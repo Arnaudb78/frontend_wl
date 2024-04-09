d127033f82a192e35eed9583fa3a0cf42767a0d6
Arnaudb78 <arnaudb92230@gmail.com>
2024-04-09 15:48:44 +0200

    DONE: create new component setting-card, css on history card component

---
+++ src/app/components/setting-card/setting-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-setting-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './setting-card.component.html',
+  styleUrl: './setting-card.component.css'
+})
+export class SettingCardComponent {
+
+}

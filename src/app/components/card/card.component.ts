499cb12be1ff4d623fbf38613ad1565423fc5c3e
Arnaudb78 <arnaudb92230@gmail.com>
2024-04-17 19:59:12 +0200

    WIP: try to build project

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -42,7 +42,7 @@ export class CardComponent implements OnInit {
     if (!data) throw new Error('No data received');
     const email = encodeURIComponent(data.email);
     const response = await fetch(
-      `http://localhost:5001/user/email?email=${email}`,
+      `http://77.37.87.171:5001/user/email?email=${email}`,
       {
         method: 'GET',
         headers: {
--- src/environnement/environnement.ts
+++ src/environnement/environnement.ts
@@ -1,6 +1,6 @@
 export const environnement = {
   production: false,
-  BACK_URL: 'http://localhost:5001',
+  BACK_URL: 'http://77.37.87.171:5001',
   GOOGLE_ID:
     '863412296645-pdjr2r8oid6tppv09gtsoi7i9ff1qkb2.apps.googleusercontent.com',
 };
